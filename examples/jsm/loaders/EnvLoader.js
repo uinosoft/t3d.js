f82a36f19972144e81a609d9408a4c3b6a71e214
Shawn Xie <shawn0326@163.com>
2023-11-01 17:55:33 +0800

    refactor: cleanup

--- examples/jsm/loaders/EnvLoader.js
+++ examples/jsm/loaders/EnvLoader.js
@@ -15,17 +15,27 @@ import {
 
 import { ReflectionProbe } from '../probes/ReflectionProbe.js';
 
-const DefaultEnvironmentTextureImageType = 'image/png';
-const CurrentVersion = 2;
-const MagicBytes = [0x86, 0x16, 0x87, 0x96, 0xf6, 0xd6, 0x96, 0x36];
-
+// This loader can be used to load .env files from BabylonJS
 export class EnvLoader extends Loader {
 
 	constructor(manager) {
 		super(manager);
+
+		this._renderer = null;
+		this._outputTexture = null;
 	}
 
-	load(options, onLoad, onProgress, onError) {
+	setRenderer(renderer) {
+		this._renderer = renderer;
+		return this;
+	}
+
+	setOutputTexture(texture) {
+		this._outputTexture = texture;
+		return this;
+	}
+
+	load(url, onLoad, onProgress, onError) {
 		const scope = this;
 
 		const loader = new FileLoader(this.manager);
@@ -34,33 +44,21 @@ export class EnvLoader extends Loader {
 		loader.setPath(this.path);
 		loader.setWithCredentials(this.withCredentials);
 
-		loader.load(
-			options.url,
-			function(buffer) {
-				if (onLoad !== undefined) {
-					const srcBuffer = new Uint8Array(
-						buffer,
-						0,
-						buffer.dataLength
-					);
-
-					onLoad(scope.parse(srcBuffer, options));
-				}
-			},
-			onProgress,
-			onError
-		);
+		loader.load(url, function(buffer) {
+			if (onLoad !== undefined) {
+				onLoad(scope.parse(buffer));
+			}
+		}, onProgress, onError);
 	}
 
-	parse(buffer, options) {
-		const info = this.GetEnvInfo(buffer);
-		const imageData = this.CreateImageDataArrayBufferViews(buffer, info);
-		const envMap = new TextureCube();
+	parse(buffer) {
+		const bufferArray = new Uint8Array(buffer, 0, buffer.dataLength);
 
-		let outputTexture = options.outputTexture;
-		if (outputTexture) {
-			outputTexture = options.outputTexture;
-		} else {
+		const info = this.getEnvInfo(bufferArray);
+		const imageData = this.createImageDataArrayBufferViews(bufferArray, info);
+
+		let outputTexture = this._outputTexture;
+		if (!outputTexture) {
 			outputTexture = new TextureCube();
 		}
 		outputTexture.type = PIXEL_TYPE.HALF_FLOAT;
@@ -73,7 +71,10 @@ export class EnvLoader extends Loader {
 		const target = new RenderTargetCube(mipmapSize, mipmapSize);
 		target.detach(ATTACHMENT.DEPTH_STENCIL_ATTACHMENT);
 		target.attach(outputTexture, ATTACHMENT.COLOR_ATTACHMENT0);
+
 		const reflectionProbe = new ReflectionProbe(target);
+
+		const envMap = new TextureCube();
 
 		const dummyScene = new Scene();
 		const geometry = new BoxGeometry(1, 1, 1);
@@ -88,6 +89,7 @@ export class EnvLoader extends Loader {
 
 		const mipmapsCount = Math.round(Log2(mipmapSize)) + 1;
 		const urls = [];
+
 		for (let i = 0; i < mipmapsCount; i++) {
 			outputTexture.mipmaps[i] = [];
 			for (let j = 0; j < 6; j++) {
@@ -106,15 +108,15 @@ export class EnvLoader extends Loader {
 			urls[i * 6 + 2] = urls[i * 6 + 3];
 			urls[i * 6 + 3] = url;
 		}
-		const promises = [];
-		urls.forEach(url => {
-			promises.push(new Promise(resolve => {
+
+		const promises = urls.map(url => {
+			return new Promise(resolve => {
 				const img = new Image();
 				img.src = url;
 				img.addEventListener('load', () => {
 					resolve(img);
 				});
-			}));
+			});
 		});
 
 		Promise.all(promises).then(images => {
@@ -122,14 +124,15 @@ export class EnvLoader extends Loader {
 				envMap.images = images.slice(i * 6, i * 6 + 6);
 				envMap.version++;
 				target.activeMipmapLevel = i;
-				reflectionProbe.render(options.renderer, dummyScene);
+				reflectionProbe.render(this._renderer, dummyScene);
 				reflectionProbe.camera.rect.z = 1 / (Math.pow(2, i + 1));
 				reflectionProbe.camera.rect.w = 1 / (Math.pow(2, i + 1));
 			}
 		});
 		return outputTexture;
 	}
-	CreateImageDataArrayBufferViews(data, info) {
+
+	createImageDataArrayBufferViews(data, info) {
 		info = this.normalizeEnvInfo(info);
 
 		const specularInfo = info.specular;
@@ -160,7 +163,8 @@ export class EnvLoader extends Loader {
 
 		return imageData;
 	}
-	GetEnvInfo(data) {
+
+	getEnvInfo(data) {
 		const dataView = new DataView(
 			data.buffer,
 			data.byteOffset,
@@ -188,12 +192,12 @@ export class EnvLoader extends Loader {
 			// Extend the header with the position of the payload.
 			manifest.specular.specularDataPosition = pos;
 			// Fallback to 0.8 exactly if lodGenerationScale is not defined for backward compatibility.
-			manifest.specular.lodGenerationScale =
-                manifest.specular.lodGenerationScale || 0.8;
+			manifest.specular.lodGenerationScale = manifest.specular.lodGenerationScale || 0.8;
 		}
 
 		return manifest;
 	}
+
 	normalizeEnvInfo(info) {
 		if (info.version > CurrentVersion) {
 			throw new Error(
@@ -216,6 +220,11 @@ export class EnvLoader extends Loader {
 	}
 
 }
+
+const DefaultEnvironmentTextureImageType = 'image/png';
+const CurrentVersion = 2;
+const MagicBytes = [0x86, 0x16, 0x87, 0x96, 0xf6, 0xd6, 0x96, 0x36];
+
 const RGBDDecodeShader = {
 	name: 'RGBDDecodePass',
 
@@ -224,37 +233,37 @@ const RGBDDecodeShader = {
 	},
 
 	vertexShader: `
-    #include <common_vert>
-    varying vec3 vDir;
-    void main() {
-        vDir = (u_Model * vec4(a_Position, 0.0)).xyz;
-        gl_Position = u_ProjectionView * u_Model * vec4(a_Position, 1.0);
-        gl_Position.z = gl_Position.w; // set z to camera.far
-    }
+		#include <common_vert>
+		varying vec3 vDir;
+		void main() {
+			vDir = (u_Model * vec4(a_Position, 0.0)).xyz;
+			gl_Position = u_ProjectionView * u_Model * vec4(a_Position, 1.0);
+			gl_Position.z = gl_Position.w; // set z to camera.far
+		}
 	`,
 
 	fragmentShader: `
-	#include <common_frag>
+		#include <common_frag>
 
         uniform samplerCube environmentMap;
 		varying vec3 vDir;
    
         vec3 fromRGBD(vec4 rgbd) {
-			return rgbd.rgb/rgbd.a;
+			return rgbd.rgb / rgbd.a;
 		}
 
 		void main() {
 			#include <begin_frag>
 
 			vec3 V = normalize(vDir);
-            vec3 coordVec = vec3( V.x, -V.yz);
+            vec3 coordVec = vec3(V.x, -V.yz);
             outColor = vec4(fromRGBD(textureCube(environmentMap, coordVec)),1.0);
 
 			#include <end_frag>
-		
 		}
 	`
 };
+
 function Log2(value) {
 	return Math.log(value) * Math.LOG2E;
 }
