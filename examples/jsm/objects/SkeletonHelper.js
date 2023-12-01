1898d09ee68ba616645af839aba29ef04deade08
liziyue <liziyue@uino.com>
2023-12-01 15:37:05 +0800

    refactor(addons): optimize SkeletonHelper

--- examples/jsm/objects/SkeletonHelper.js
+++ examples/jsm/objects/SkeletonHelper.js
@@ -1,96 +1,142 @@
 import {
 	Attribute,
 	Buffer,
-	Color3,
 	Geometry,
-	LineMaterial,
 	Matrix4,
 	Mesh,
-	VERTEX_COLOR,
+	Object3D,
+	SphereGeometry,
+	ShaderMaterial,
 	Vector3
 } from 't3d';
 
-class SkeletonHelper extends Mesh {
+class SkeletonHelper extends Object3D {
 
 	constructor(object) {
+		super();
+
 		const bones = getBoneList(object);
 
-		const geometry = new Geometry();
+		this.root = object;
+		this.bones = bones;
 
-		const vertices = [];
-		const colors = [];
+		this._midStep = 0.25;
 
-		const color1 = new Color3(0, 0, 1);
-		const color2 = new Color3(0, 1, 0);
+		this.midWidthScale = 0.1;
+		this.ballScale = 0.4;
+
+		// create bone spheres and connectors
+
+		const material = new ShaderMaterial(skeletonShader);
+		material.depthTest = false;
+		material.depthWrite = false;
+		this._material = material;
+
+		this._sphereGeometry = new SphereGeometry(0.25, 16, 12);
+		this._connectorGeometry = new ConnectorGeometry();
+
+		this._distanceMap = new WeakMap();
 
 		for (let i = 0; i < bones.length; i++) {
 			const bone = bones[i];
 
 			if (bone.parent && bone.parent.isBone) {
-				vertices.push(0, 0, 0);
-				vertices.push(0, 0, 0);
-				colors.push(color1.r, color1.g, color1.b, 1);
-				colors.push(color2.r, color2.g, color2.b, 1);
+				const boneSphere = new Mesh(this._sphereGeometry, this._material);
+				boneSphere.receiveShadows = false;
+				boneSphere.castShadows = false;
+				boneSphere.renderOrder = 1;
+				this.add(boneSphere);
+
+				const boneConnector = new Mesh(this._connectorGeometry, this._material);
+				boneConnector.receiveShadows = false;
+				boneConnector.castShadows = false;
+				this.add(boneConnector);
+
+				this._distanceMap.set(bone.parent, 0);
 			}
 		}
+	}
 
-		geometry.addAttribute('a_Position', new Attribute(new Buffer(new Float32Array(vertices), 3)));
-		geometry.addAttribute('a_Color', new Attribute(new Buffer(new Float32Array(colors), 4)));
+	set midStep(value) {
+		this._midStep = value;
+		this._connectorGeometry.updateMidStep(value);
+	}
 
-		const material = new LineMaterial();
-		material.vertexColors = VERTEX_COLOR.RGBA;
-		material.depthTest = false;
-		material.depthWrite = false;
-		material.transparent = true;
+	get midStep() {
+		return this._midStep;
+	}
 
-		super(geometry, material);
+	set colorMin(value) {
+		this._material.uniforms.u_colorMin = value;
+	}
 
-		this.frustumCulled = false;
+	get colorMin() {
+		return this._material.uniforms.u_colorMin;
+	}
 
-		this.root = object;
-		this.bones = bones;
+	set colorMax(value) {
+		this._material.uniforms.u_colorMax = value;
+	}
+
+	get colorMax() {
+		return this._material.uniforms.u_colorMax;
 	}
 
 	updateMatrix(force) {
 		const bones = this.bones;
+		const midWidthScale = this.midWidthScale;
+		const ballScale = this.ballScale;
 
-		const geometry = this.geometry;
-		const position = geometry.getAttribute('a_Position');
+		const distanceMap = this._distanceMap;
 
-		worldMatrixInv.getInverse(this.root.worldMatrix);
+		_worldMatrixInv.getInverse(this.root.worldMatrix);
 
 		for (let i = 0, j = 0; i < bones.length; i++) {
 			const bone = bones[i];
 
 			if (bone.parent && bone.parent.isBone) {
-				boneMatrix.multiplyMatrices(worldMatrixInv, bone.worldMatrix);
-				vector.setFromMatrixPosition(boneMatrix);
+				_boneEndMatrix.multiplyMatrices(_worldMatrixInv, bone.worldMatrix);
+				_boneStartMatrix.multiplyMatrices(_worldMatrixInv, bone.parent.worldMatrix);
+				_boneEnd.setFromMatrixPosition(_boneEndMatrix);
+				_boneStart.setFromMatrixPosition(_boneStartMatrix);
 
-				position.buffer.array[j * position.size + 0] = vector.x;
-				position.buffer.array[j * position.size + 1] = vector.y;
-				position.buffer.array[j * position.size + 2] = vector.z;
+				_boneDirection.subVectors(_boneEnd, _boneStart);
+				const boneDistance = _boneDirection.getLength();
+				_boneDirection.normalize();
 
-				boneMatrix.multiplyMatrices(worldMatrixInv, bone.parent.worldMatrix);
-				vector.setFromMatrixPosition(boneMatrix);
+				bone._distance = boneDistance;
+				distanceMap.set(bone, boneDistance);
 
-				position.buffer.array[(j + 1) * position.size + 0] = vector.x;
-				position.buffer.array[(j + 1) * position.size + 1] = vector.y;
-				position.buffer.array[(j + 1) * position.size + 2] = vector.z;
+				let minDistance = boneDistance;
+				const parentDistance = distanceMap.get(bone.parent);
+				if (parentDistance) {
+					minDistance = Math.min(minDistance, parentDistance);
+				}
+
+				const sphere = this.children[j];
+				sphere.position.copy(_boneStart);
+				sphere.scale.set(1, 1, 1).multiplyScalar(minDistance * ballScale);
+
+				const connector = this.children[j + 1];
+				connector.position.copy(_boneStart);
+				connector.scale.set(midWidthScale, midWidthScale, 1).multiplyScalar(boneDistance);
+				connector.quaternion.setFromUnitVectors(new Vector3(0, 0, -1), _boneDirection);
 
 				j += 2;
 			}
 		}
-
-		position.buffer.version++;
 
 		super.updateMatrix(force);
 	}
 
 }
 
-const vector = new Vector3();
-const boneMatrix = new Matrix4();
-const worldMatrixInv = new Matrix4();
+const _worldMatrixInv = new Matrix4();
+const _boneStartMatrix = new Matrix4();
+const _boneEndMatrix = new Matrix4();
+const _boneStart = new Vector3();
+const _boneEnd = new Vector3();
+const _boneDirection = new Vector3();
 
 function getBoneList(object) {
 	const boneList = [];
@@ -105,5 +151,108 @@ function getBoneList(object) {
 
 	return boneList;
 }
+
+class ConnectorGeometry extends Geometry {
+
+	constructor(midStep = 0.25) {
+		super();
+
+		const vertexs = [
+			new Vector3(0, 0, 0),
+			new Vector3(1, -1, -midStep),
+			new Vector3(1, 1, -midStep),
+			new Vector3(-1, 1, -midStep),
+			new Vector3(-1, -1, -midStep),
+			new Vector3(1, -1, -midStep),
+			new Vector3(0, 0, -1)
+		];
+
+		const normal = [
+			new Vector3(1, 0, 1),
+			new Vector3(0, 1, 1),
+			new Vector3(-1, 0, 1),
+			new Vector3(0, -1, 1),
+			new Vector3(1, 0, -1),
+			new Vector3(0, 1, -1),
+			new Vector3(-1, 0, -1),
+			new Vector3(0, -1, -1)
+		];
+
+		const positions = [];
+		const normals = [];
+
+		for (let i = 0; i < 4; i++) {
+			positions.push(vertexs[0].x, vertexs[0].y, vertexs[0].z);
+			positions.push(vertexs[i + 1].x, vertexs[i + 1].y, vertexs[i + 1].z);
+			positions.push(vertexs[i + 2].x, vertexs[i + 2].y, vertexs[i + 2].z);
+			normals.push(normal[i].x, normal[i].y, normal[i].z);
+			normals.push(normal[i].x, normal[i].y, normal[i].z);
+			normals.push(normal[i].x, normal[i].y, normal[i].z);
+
+			positions.push(vertexs[6].x, vertexs[6].y, vertexs[6].z);
+			positions.push(vertexs[i + 2].x, vertexs[i + 2].y, vertexs[i + 2].z);
+			positions.push(vertexs[i + 1].x, vertexs[i + 1].y, vertexs[i + 1].z);
+			normals.push(normal[i + 4].x, normal[i + 4].y, normal[i + 4].z);
+			normals.push(normal[i + 4].x, normal[i + 4].y, normal[i + 4].z);
+			normals.push(normal[i + 4].x, normal[i + 4].y, normal[i + 4].z);
+		}
+
+		this.addAttribute('a_Position', new Attribute(new Buffer(new Float32Array(positions), 3)));
+		this.addAttribute('a_Normal', new Attribute(new Buffer(new Float32Array(normals), 3)));
+
+		this.computeBoundingSphere();
+		this.computeBoundingBox();
+	}
+
+	updateMidStep(midStep) {
+		const positions = this.getAttribute('a_Position').buffer.array;
+
+		for (let i = 0; i < 8; i++) {
+			positions[i * 9 + 5] = -midStep;
+			positions[i * 9 + 8] = -midStep;
+		}
+
+		this.getAttribute('a_Position').buffer.version++;
+
+		// boundings are not needed to be updated
+	}
+
+}
+
+const skeletonShader = {
+	name: 'skeleton_shader',
+
+	uniforms: {
+		'u_colorMin': [0.35, 0.35, 0.35],
+		'u_colorMax': [0.7, 0.7, 0.7]
+	},
+
+	vertexShader: `
+        #include <common_vert>
+
+		varying vec3 v_Normal;
+
+		void main() {
+			v_Normal = (transposeMat4(inverseMat4(u_Model)) * vec4(a_Normal, 0.0)).xyz;
+			gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 1.0);
+		}
+    `,
+
+	fragmentShader: `
+        #include <common_frag>
+
+		uniform vec3 u_colorMin;
+		uniform vec3 u_colorMax;
+
+		varying vec3 v_Normal;
+
+		void main() {
+            vec3 N = normalize(v_Normal);
+			float ndl = dot(N, vec3(0, 1, 0)) * 0.5 + 0.5;
+			vec3 diffuse = mix(u_colorMin, u_colorMax, ndl);
+			gl_FragColor = vec4(diffuse, 1.0);
+		}
+    `
+};
 
 export { SkeletonHelper };
