d42c5b96f10dc36c8061f66f9185067b1dd612d3
Lixc <1141074339@qq.com>
2024-06-19 15:34:03 +0800

    feat(addons): add `generateStartCap` and `generateEndCap` options for `TubeBuilder`

--- examples/jsm/geometries/builders/TubeBuilder.js
+++ examples/jsm/geometries/builders/TubeBuilder.js
@@ -11,14 +11,18 @@ const TubeBuilder = {
 	 * @param {Number} [options.radius=0.1]
 	 * @param {Number} [options.radialSegments=8]
 	 * @param {Number} [options.startRad=0]
+	 * @param {Boolean} [options.generateStartCap=false] - Whether to generate the start cap.
+	 * @param {Boolean} [options.generateEndCap=false] - Whether to generate the end cap.
 	 * @return {Object} - The geometry data.
 	 */
 	getGeometryData: function(frames, options = {}) {
 		const radius = options.radius !== undefined ? options.radius : 0.1;
 		const radialSegments = Math.max(2, options.radialSegments !== undefined ? options.radialSegments : 8);
 		const startRad = options.startRad !== undefined ? options.startRad : 0;
+		const generateStartCap = (options.generateStartCap !== undefined) ? options.generateStartCap : false;
+		const generateEndCap = (options.generateEndCap !== undefined) ? options.generateEndCap : false;
 
-		// build vertex data
+		// build side vertex data
 
 		const positions = [];
 		const normals = [];
@@ -71,6 +75,46 @@ const TubeBuilder = {
 						begin2 + i, begin1 + i + 1, begin2 + i + 1
 					);
 				}
+			}
+		}
+
+		// end cap
+
+		if (radialSegments >= 3 && generateEndCap) {
+			normalVector.copy(frames.tangents[lastIndex]);
+			normalVector.normalize();
+
+			for (let r = verticesCount - radialSegments, l = verticesCount; r < l; r++) {
+				positions.push(positions[r * 3], positions[r * 3 + 1], positions[r * 3 + 2]);
+				uvs.push(uvs[r * 2], uvs[r * 2 + 1]);
+				uvs2.push(uvs2[r * 2], uvs2[r * 2 + 1]);
+				normals.push(normalVector.x, normalVector.y, normalVector.z);
+				verticesCount++;
+			}
+
+			const index = verticesCount - radialSegments;
+			for (let i = 0; i < radialSegments - 2; i++) {
+				indices.push(index, index + i + 1, index + i + 2);
+			}
+		}
+
+		// start cap
+
+		if (radialSegments >= 3 && generateStartCap) {
+			normalVector.copy(frames.tangents[0]);
+			normalVector.normalize();
+
+			for (let r = 0; r < radialSegments; r++) {
+				positions.push(positions[r * 3], positions[r * 3 + 1], positions[r * 3 + 2]);
+				normals.push(-normalVector.x, -normalVector.y, -normalVector.z);
+				uvs.push(uvs[r * 2], uvs[r * 2 + 1]);
+				uvs2.push(uvs2[r * 2], uvs2[r * 2 + 1]);
+				verticesCount++;
+			}
+
+			const index = verticesCount - radialSegments;
+			for (let i = 0; i < radialSegments - 2; i++) {
+				indices.push(index, index + i + 2, index + i + 1);
 			}
 		}
 
