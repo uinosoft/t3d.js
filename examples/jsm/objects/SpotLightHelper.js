d55a9ca3b35e59886663b42956ad8db5843fe2d4
liziyue <liziyue@uino.com>
2024-08-06 15:23:09 +0800

    refactor(addons): update `SpotLightHelper`

--- examples/jsm/objects/SpotLightHelper.js
+++ examples/jsm/objects/SpotLightHelper.js
@@ -18,23 +18,7 @@ class SpotLightHelper extends Object3D {
 
 		const geometry = new Geometry();
 
-		const positions = [
-			0, 0, 0, 	0, 0, -1,
-			0, 0, 0, 	1, 0, -1,
-			0, 0, 0,	-1, 0, -1,
-			0, 0, 0, 	0, 1, -1,
-			0, 0, 0, 	0, -1, -1
-		];
-
-		for (let i = 0, j = 1, l = 32; i < l; i++, j++) {
-			const p1 = (i / l) * Math.PI * 2;
-			const p2 = (j / l) * Math.PI * 2;
-
-			positions.push(
-				Math.cos(p1), Math.sin(p1), -1,
-				Math.cos(p2), Math.sin(p2), -1
-			);
-		}
+		const positions = this.getPositions();
 
 		geometry.addAttribute('a_Position', new Attribute(new Buffer(new Float32Array(positions), 3)));
 
@@ -49,11 +33,66 @@ class SpotLightHelper extends Object3D {
 		this.update();
 	}
 
-	update() {
-		const coneLength = this.light.distance ? this.light.distance : 1000;
-		const coneWidth = coneLength * Math.tan(this.light.angle);
+	getPositions() {
+		const angle = this.light.angle;
+		const radius = Math.sin(angle);
+		const z = Math.cos(angle);
+		this.angle = angle;
 
-		this.cone.scale.set(coneWidth, coneWidth, coneLength);
+		const positions = [
+			0, 0, 0, 	0, 0, -1,
+			0, 0, 0, 	radius, 0, -z,
+			0, 0, 0,	-radius, 0, -z,
+			0, 0, 0, 	0, radius, -z,
+			0, 0, 0, 	0, -radius, -z
+		];
+
+		for (let i = 0, j = 1, l = 32; i < l; i++, j++) {
+			const p1 = (i / l) * Math.PI * 2;
+			const p2 = (j / l) * Math.PI * 2;
+
+			positions.push(
+				Math.cos(p1) * radius, Math.sin(p1) * radius, -z,
+				Math.cos(p2) * radius, Math.sin(p2) * radius, -z
+			);
+		}
+
+		const startRadian = -Math.PI / 2 - angle;
+		for (let i = 0, j = 1, l = 32; i < l; i++, j++) {
+			const p1 = (i / l) * angle * 2 + startRadian;
+			const p2 = (j / l) * angle * 2 + startRadian;
+
+			positions.push(
+				0, Math.cos(p1), Math.sin(p1),
+				0, Math.cos(p2), Math.sin(p2),
+				Math.cos(p1), 0, Math.sin(p1),
+				Math.cos(p2), 0, Math.sin(p2)
+			);
+		}
+
+		return positions;
+	}
+
+	update() {
+		if (this.angle !== this.light.angle) {
+			const positions = this.getPositions();
+
+			const geometry = this.cone.geometry;
+			const positionBuffer = geometry.attributes.a_Position.buffer;
+			const array = positionBuffer.array;
+
+			for (let i = 0, l = positions.length; i < l; i++) {
+				array[i] = positions[i];
+			}
+
+			positionBuffer.version++;
+			geometry.computeBoundingBox();
+			geometry.computeBoundingSphere();
+		}
+
+		const scaleFactor = this.light.distance ? this.light.distance : 1000;
+
+		this.cone.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
 		if (this.color !== undefined) {
 			this.cone.material.diffuse.setHex(this.color);
